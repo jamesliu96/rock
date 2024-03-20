0c3df240be70907545d4d905de3748b85a6825ec
James Liu <jamesliu96@users.noreply.github.com>
2024-03-20 15:07:37 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,172 @@
+type Optional<T> = {
+  [P in keyof T]?: T[P] | null;
+};
+
+type Play = Optional<{
+  next: string;
+  previous: string;
+  results: Optional<{
+    id: number;
+    uri: string;
+    airdate: string;
+    show: Optional<{
+      id: number;
+      uri: string;
+      program: number;
+      program_uri: string;
+      hosts: number[];
+      host_uris: string[];
+      program_name: string;
+      program_tags: string;
+      host_names: string[];
+      tagline: string;
+      image_uri: string;
+      start_time: string;
+    }>;
+    show_uri: string;
+    image_uri: string;
+    thumbnail_uri: string;
+    song: string;
+    track_id: string;
+    recording_id: string;
+    artist: string;
+    artist_ids: string[];
+    album: string;
+    release_id: string;
+    release_group_id: string;
+    labels: string[];
+    label_ids: string[];
+    release_date: string;
+    rotation_status: string;
+    is_local: boolean;
+    is_request: boolean;
+    is_live: boolean;
+    comment: string;
+    play_type: string;
+  }>[];
+}>;
+
+const sleep = (ms: number) =>
+  new Promise<void>((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+const main = async () => {
+  let booster = () => {};
+  document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+      booster();
+    }
+  });
+  const { body, head } = document;
+  body.addEventListener('click', () => {
+    booster();
+  });
+  body.style.margin = '0px';
+  body.style.minHeight = '100vh';
+  body.style.display = 'flex';
+  body.style.flexDirection = 'column';
+  body.style.justifyContent = 'center';
+  body.style.alignItems = 'center';
+  body.style.backgroundPosition = 'center';
+  body.style.backgroundSize = 'cover';
+  body.style.backgroundRepeat = 'no-repeat';
+  body.style.backdropFilter = 'blur(16px) brightness(0.5)';
+  body.style.color = 'white';
+  body.style.fontFamily = 'monospace';
+  body.style.textAlign = 'center';
+  body.style.lineHeight = '1.5';
+  body.style.padding = '0px 10px';
+  const $style = document.createElement('style');
+  head.append($style);
+  $style.sheet?.insertRule('* { box-sizing: border-box; }');
+  const $cover = document.createElement('img');
+  const $show = document.createElement('div');
+  const $artist = document.createElement('div');
+  const $song = document.createElement('div');
+  const $album = document.createElement('div');
+  const $release = document.createElement('div');
+  const $label = document.createElement('div');
+  const $comment = document.createElement('div');
+  const $tagline = document.createElement('div');
+  const $type = document.createElement('div');
+  body.append(
+    $cover,
+    $show,
+    $artist,
+    $song,
+    $album,
+    $release,
+    $label,
+    $comment,
+    $tagline,
+    $type
+  );
+  $cover.style.border = '1px solid #ccc';
+  $cover.style.maxWidth = '100%';
+  $show.style.fontSize = 'small';
+  $artist.style.fontWeight = 'bold';
+  $song.style.fontStyle = 'italic';
+  $release.style.fontSize = 'smaller';
+  $label.style.fontSize = 'x-small';
+  $tagline.style.fontSize = 'xx-small';
+  $comment.style.fontSize = 'xx-small';
+  $type.style.fontSize = 'xx-small';
+  const $init = document.createElement('div');
+  body.append($init);
+  $init.textContent = '⏵';
+  $init.style.width = '100%';
+  $init.style.height = '100%';
+  $init.style.display = 'flex';
+  $init.style.justifyContent = 'center';
+  $init.style.alignItems = 'center';
+  $init.style.position = 'fixed';
+  $init.style.top = '0px';
+  $init.style.left = '0px';
+  $init.style.fontSize = 'calc(min(100vw, 100vh))';
+  $init.style.lineHeight = '1';
+  $init.style.background = 'rgb(0 0 0 / 50%)';
+  $init.style.overflow = 'hidden';
+  $init.style.userSelect = 'none';
+  $init.style.pointerEvents = 'none';
+  const handle = () => {
+    body.removeEventListener('click', handle);
+    $init.remove();
+    new Audio('https://kexp.streamguys1.com/kexp320.mp3').play();
+  };
+  body.addEventListener('click', handle);
+  for (
+    let boost: Promise<void>;
+    ;
+    boost = new Promise<void>((resolve) => {
+      booster = resolve;
+    }),
+      await Promise.race([sleep(30000), boost])
+  ) {
+    const play = (
+      (await (
+        await fetch(
+          `https://api.kexp.org/v2/plays/?expand=show&format=json&limit=1&ordering=-airdate&airdate_before=${new Date().toISOString()}`
+        )
+      ).json()) as Play | undefined
+    )?.results?.[0];
+    if (!play) return;
+    const url = play.image_uri || play.thumbnail_uri || play.show?.image_uri;
+    body.style.backgroundImage = `url(${url})`;
+    $cover.src = `${url}`;
+    const hosts = play.show?.host_names?.join(' & ');
+    $show.textContent = `${play.show?.program_name ?? ''}${
+      hosts ? ` with ${hosts}` : ''
+    }`;
+    $artist.textContent = play.artist ?? '';
+    $song.textContent = play.song ?? '';
+    $album.textContent = play.album ?? '';
+    $release.textContent = play.release_date ?? '';
+    $label.textContent = play.labels?.join(' & ') ?? '';
+    $comment.textContent = play.comment ?? '';
+    $tagline.textContent = play.show?.tagline ?? '';
+    $type.textContent = play.play_type ?? '';
+  }
+};
+
+main();
